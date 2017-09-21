ce823ab1a01fdb50248d034c593e5c3392558c25
lance@4hilton.com <lance@lancetek.com>
2017-09-21 11:50:14 -0700

    aspnetuser fixed

--- WebApplication1/Models/AspNetUserRole.cs
+++ WebApplication1/Models/AspNetUserRole.cs
@@ -10,10 +10,15 @@
 namespace WebApplication1.Models
 {
     using System;
+    using System.Collections.Generic;
     
-    public partial class sp_helpdiagramdefinition_Result
+    public partial class AspNetUserRole
     {
-        public Nullable<int> version { get; set; }
-        public byte[] definition { get; set; }
+        public string UserId { get; set; }
+        public string RoleId { get; set; }
+        public int ID { get; set; }
+    
+        public virtual AspNetRole AspNetRole { get; set; }
+        public virtual AspNetUser AspNetUser { get; set; }
     }
 }
